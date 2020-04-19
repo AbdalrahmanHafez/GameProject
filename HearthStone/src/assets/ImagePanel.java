b6b6476895f4b1183f5940aeed1011bcae198c4e
AboDa7aM <dahomy999@outlook.sa>
2020-04-19 13:18:20 +0200

    update

--- HearthStone/src/assets/ImagePanel.java
+++ HearthStone/src/assets/ImagePanel.java
@@ -49,9 +49,7 @@ public class ImagePanel extends JPanel
     protected void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                            RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
         g.drawImage(scaledImage, 0,0 , this);
     }
   
