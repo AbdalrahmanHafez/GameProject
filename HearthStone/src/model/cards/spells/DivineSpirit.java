9baa7abe515b64f10de39b6762cfa31120a6a914
AboDa7aM <dahomy999@outlook.sa>
2020-05-12 09:28:21 +0200

    DemoVideo added

--- HearthStone/src/model/cards/spells/DivineSpirit.java
+++ HearthStone/src/model/cards/spells/DivineSpirit.java
@@ -6,7 +6,7 @@ import model.cards.minions.Minion;
 public class DivineSpirit extends Spell implements MinionTargetSpell {
 
 	public DivineSpirit() {
-		super("Divine Spirit", 3, Rarity.BASIC);
+		super("Divine Spirit", 2, Rarity.BASIC);
 		super.setCardImage("resources/images/Spells/DivineSpirit.png");
 		super.setAvatar("resources/images/Spells/avatar/DivineSpirit.png");
 
